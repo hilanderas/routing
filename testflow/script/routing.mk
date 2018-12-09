654015638495e4f7da56ed0037b62bbe3485c6ba
brownyuan <brown.yuan@dorry.space>
2018-12-09 16:47:03 +0800

    fix issue in test flow

--- testflow/script/routing.mk
+++ testflow/script/routing.mk
@@ -7,7 +7,9 @@ include .env
 #	sed -i s/TEST_ROUTE_NUM\c/export TEST_ROUTE_NUM=${NUM}/g
 
 create-proj-18-s-dhcp:
-	make -C ${TEST_PROJ} create-dev OS_VERSION=18.04 WAN_MODE=dhcp ROUTE_NUM=single
+	make -C ${TEST_PROJ} create-dev OS_VERSION=18.04 WAN_MODE=dhcp ROUTE_NUM=single || true
+	#sed -i '/TEST_RPOJ/c\export TEST_PROJ=dev-18.04-dhcp-single' .env
+	sed -i '/TEST_PROJ/c\export TEST_PROJ=../../dev-18.04-dhcp-single' .env
 create-proj-16-s-dhcp:
-	make -C ${TEST_PROJ} create-dev OS_VERSION=16.04 WAN_MODE=dhcp ROUTE_NUM=single
-
+	make -C ${TEST_PROJ} create-dev OS_VERSION=16.04 WAN_MODE=dhcp ROUTE_NUM=single || true
+	sed -i s/TEST_RPOJ\c/export TEST_PROJ=dev-16.04-dhcp-single/g .env
