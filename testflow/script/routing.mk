937bf300cb4604334d882e1187c6399520fe3bff
AP1001 <Alejandro.Perez1001@protonmail.com>
2018-12-10 10:45:13 +0800

    fix restart issue in test flow; add more packages for dev mode

--- testflow/script/routing.mk
+++ testflow/script/routing.mk
@@ -9,16 +9,32 @@ ROUTING_PROJ=../..
 
 create-proj-18-s-dhcp: 
 	make -C ${ROUTING_PROJ} create-dev OS_VERSION=18.04 WAN_MODE=dhcp ROUTE_NUM=single || true
-	#sed -i '/TEST_RPOJ/c\export TEST_PROJ='dev-18.04-dhcp-single .env
 	sed -i '/TEST_PROJ/c\export TEST_PROJ=../../dev-18.04-dhcp-single' .env
 	cat .env
 create-proj-16-s-dhcp:
 	make -C ${ROUTING_PROJ} create-dev OS_VERSION=16.04 WAN_MODE=dhcp ROUTE_NUM=single || true
 	sed -i '/TEST_PROJ/c\export TEST_PROJ=../../dev-16.04-dhcp-single' .env
 	cat .env
+create-proj-16-d-pppoe: 
+	make -C ${ROUTING_PROJ} create-dev OS_VERSION=16.04 WAN_MODE=pppoe ROUTE_NUM=dual || true
+	sed -i '/TEST_PROJ/c\export TEST_PROJ=../../dev-16.04-pppoe-dual' .env
+	cat .env
+create-proj-16-s-pppoe:
+	make -C ${ROUTING_PROJ} create-dev OS_VERSION=16.04 WAN_MODE=pppoe ROUTE_NUM=single || true
+	sed -i '/TEST_PROJ/c\export TEST_PROJ=../../dev-16.04-pppoe-single' .env
+	cat .env
+
 del-proj-18-s-dhcp:
 	rm -rf ${ROUTING_PROJ}/dev-18.04-dhcp-single
 	make -f basic.mk reset_config
 del-proj-16-s-dhcp:
 	rm -rf ${ROUTING_PROJ}/dev-16.04-dhcp-single
 	make -f basic.mk reset_config
+del-proj-16-dual-pppoe:
+	rm -rf ${ROUTING_PROJ}/dev-16.04-pppoe-dual
+	make -f basic.mk reset_config
+del-proj-16-s-pppoe:
+	rm -rf ${ROUTING_PROJ}/dev-16.04-pppoe-single
+	make -f basic.mk reset_config
+
+
